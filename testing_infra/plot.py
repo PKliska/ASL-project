a01ade09eb0889b5301bba0ea72b762cf2c6952a
Filip Jaksic <jaksicf@student.ethz.ch>
2024-04-25 16:14:30 +0200

    aesthetics(timing plot): for X axis show only iterations for which we run the algo

--- testing_infra/plot.py
+++ testing_infra/plot.py
@@ -15,14 +15,20 @@ def plot_csv_files(csv_files, labels):
         df['n_cycles'] /= FREQUENCY
         sns.lineplot(data=df, x="n_simulation_iterations", y="n_cycles", label=label, color=color)
 
-    plt.xlabel("Sizes")
+    plt.xlabel("n_simulation_iterations")
     plt.ylabel("Runtime [s]")
     plt.legend()
     plt.grid(axis='y', alpha=0.7)
 
-    plt.savefig("plot")
+    # add show ticks for n_simulation_iterations for which we run the algo
+    plt.xticks(df["n_simulation_iterations"])
 
-    plt.show()
+    # add vertical lines at these places so that it's easier to see
+    for x in df["n_simulation_iterations"]:
+        plt.axvline(x=x, color='w', linestyle='-', linewidth=1)
+
+    plt.savefig("plot")
+    # plt.show()
 
 csv_files = ["baseline.csv", "preallocated.csv"]  # List of CSV files
 custom_labels = ["Baseline", "Preallocated"]  # List of custom labels
