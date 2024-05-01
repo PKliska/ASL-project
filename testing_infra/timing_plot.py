75e0d4492c67b3ac8386f57dba5c64239d3c9b16
Peter Horcic <phorcic@student.ethz.ch>
2024-05-01 18:20:45 +0200

    make plots performance plots not just runtime

--- testing_infra/timing_plot.py
+++ testing_infra/timing_plot.py
@@ -1,3 +1,7 @@
+
+
+
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -11,12 +15,16 @@ def plot_csv_files(csv_files, labels):
     palette = sns.color_palette("rocket", len(csv_files))
 
     for csv_file, label, color in zip(csv_files, labels, palette):
+        # flops (91 * (nx-2)*(ny-2) + 22 * (nx-2)(ny-2)*pit + 4) * steps
+        # for us nx=ny=41 and pit = 50, so
+        # num_flops = 1811515 * steps
         df = pd.read_csv(csv_file)
-        df['n_cycles'] /= FREQUENCY
+        performance = 1811515 * df['n_simulation_iterations'] / df['n_cycles'] 
+        df['n_cycles'] = performance
         sns.lineplot(data=df, x="n_simulation_iterations", y="n_cycles", label=label, color=color)
 
     plt.xlabel("n_simulation_iterations")
-    plt.ylabel("Runtime [s]")
+    plt.ylabel("Performance")
     plt.legend()
     plt.grid(axis='y', alpha=0.7)
 
@@ -28,8 +36,9 @@ def plot_csv_files(csv_files, labels):
         plt.axvline(x=x, color='w', linestyle='-', linewidth=1)
 
     plt.savefig("plot")
-    # plt.show()
+    plt.show()
 
 csv_files = ["baseline.csv", "preallocated.csv"]  # List of CSV files
 custom_labels = ["Baseline", "Preallocated"]  # List of custom labels
 plot_csv_files(csv_files, custom_labels)
+
