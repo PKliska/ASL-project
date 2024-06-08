633e7881fc0485dfd6914c5cb32da09dd64147dc
Peter Horcic <phorcic@student.ethz.ch>
2024-06-08 21:22:07 +0200

    tried to vectorize it, didnt work

--- c_implementation/include/trapeze_macros.h
+++ c_implementation/include/trapeze_macros.h
@@ -1,21 +1,25 @@
 #include "utils.h"
+#include <immintrin.h>
 
 #define TRAPEZE_LOOP_UTD for(int tx=tx0;tx<tx1;tx++)
 #define TRAPEZE_LOOP_DTU for(int tx=tx1-1;tx>=tx0;tx--)
 #define TRAPEZE_LOOP_LTR for(int ty=ty0;ty<ty1;ty++)
-#define TRAPEZE_LOOP_RTL for(int ty=ty1-1;ty>=ty0;ty--)
+#define TRAPEZE_LOOP_RTL for(int ty=ty0;ty<ty1;ty+=4)//for(int ty=ty1-1;ty>=ty0;ty--)
 #define AFTER_LEFT new[tx*d + 0] = new[tx*d + 1]
 #define AFTER_RIGHT new[tx*d + d - 1] = new[tx*d + d - 2]
 #define AFTER_TOP memcpy(new + 0*d+ty0, new + 1*d+ty0, (ty1-ty0)*sizeof(double))
 #define AFTER_BOTTOM memset(new + (d-1)*d+ty0, 0, (ty1-ty0)*sizeof(double))
 #define EMPTY_AFTER 
+#define ADDITIVE_LOOP ty
+#define SUBTRACTIVE_LOOP ty
+
 
 #define DO_TRAPEZE_GENERAL(loop1, loop2,                                      \
                            after_loop1, after_loop2,                          \
                            b, p0, p1, d,                                      \
                            t0, t1,                                            \
                            x0, dx0, x1, dx1,                                  \
-                           y0, dy0, y1, dy1)                                  \
+                           y0, dy0, y1, dy1, loop_type)                                  \
 {                                                                             \
     double *restrict old, *restrict new;                                      \
     if(t0 % 2 == 0){                                                          \
@@ -26,13 +30,28 @@
         old = p0;                                                             \
     }                                                                         \
     int tx0 = x0, tx1 = x1;                                                   \
-    int ty0 = y0, ty1 = y1;                                                   \
+    int ty0 = y0, ty1 = y1;                                                     \
+    __m256d constant_quarter = _mm256_set1_pd(0.25);                            \
+    __m256d   old_top, old_bot, old_left, old_right, b_vec, res1, res2;                                        \
     for(unsigned t=t0;t<(unsigned)t1;t++){                                    \
         loop1{                                                                \
-            loop2{                                                            \
-                new[tx*d + ty] = (old[(tx+1)*d + ty] + old[(tx-1)*d + ty]     \
-                                + old[tx*d + (ty+1)] + old[tx*d + (ty-1)]     \
-                                - b[tx*d+ty]) * 0.25;                         \
+            loop2{                                                           \
+                                                                                        \
+                old_top =  _mm256_loadu_pd(&old[(tx+1)*d + ty]);                        \
+                old_bot = _mm256_loadu_pd(&old[(tx-1)*d + ty]);                                         \
+                old_left = _mm256_loadu_pd(&old[(tx)*d + ty+1]);                                \
+                old_right = _mm256_loadu_pd(&old[(tx)*d + ty-1]);                       \
+                b_vec = _mm256_loadu_pd(&b[tx*d+ty]);                   \
+                                                                                    \
+                res1 = _mm256_add_pd(old_top, old_bot);                                     \
+                res2 =  _mm256_add_pd(old_left, old_right);                         \
+                res1 =   _mm256_add_pd(res1, res2);                                         \
+                res1 =  _mm256_sub_pd( res1, b_vec);                                    \
+                                                                                        \
+                res1 = _mm256_mul_pd(res1, constant_quarter);                           \
+                                                                                            \
+                _mm256_storeu_pd(&new[tx*d + ty], res1);                                 \
+                                                                                        \
             }                                                                 \
             after_loop2;                                                      \
         }                                                                     \
@@ -51,7 +70,7 @@ DO_TRAPEZE_GENERAL(TRAPEZE_LOOP_DTU, TRAPEZE_LOOP_RTL,                        \
                    b, p0, p1, d,                                              \
                    t0, t1,                                                    \
                    x0, dx0, x1, dx1,                                          \
-                   y0, dy0, y1, dy1)                                          \
+                   y0, dy0, y1, dy1, SUBTRACTIVE_LOOP)                                          \
 
 #define DO_TRAPEZE_TOP(b, p0, p1, d,                                          \
                            t0, t1,                                            \
@@ -62,7 +81,7 @@ DO_TRAPEZE_GENERAL(TRAPEZE_LOOP_DTU, TRAPEZE_LOOP_LTR,                        \
                    b, p0, p1, d,                                              \
                    t0, t1,                                                    \
                    x0, dx0, x1, dx1,                                          \
-                   y0, dy0, y1, dy1)                                          \
+                   y0, dy0, y1, dy1, ADDITIVE_LOOP)                                          \
 
 
 #define DO_TRAPEZE_TOP_RIGHT(b, p0, p1, d,                                    \
@@ -74,7 +93,7 @@ DO_TRAPEZE_GENERAL(TRAPEZE_LOOP_DTU, TRAPEZE_LOOP_LTR,                        \
                    b, p0, p1, d,                                              \
                    t0, t1,                                                    \
                    x0, dx0, x1, dx1,                                          \
-                   y0, dy0, y1, dy1)                                          \
+                   y0, dy0, y1, dy1, ADDITIVE_LOOP)                                          \
 
 #define DO_TRAPEZE_LEFT(b, p0, p1, d,                                         \
                            t0, t1,                                            \
@@ -85,7 +104,7 @@ DO_TRAPEZE_GENERAL(TRAPEZE_LOOP_UTD, TRAPEZE_LOOP_RTL,                        \
                    b, p0, p1, d,                                              \
                    t0, t1,                                                    \
                    x0, dx0, x1, dx1,                                          \
-                   y0, dy0, y1, dy1)                                          \
+                   y0, dy0, y1, dy1, SUBTRACTIVE_LOOP)                                          \
 
 
 #define DO_TRAPEZE_MID(b, p0, p1, d,                                          \
@@ -97,7 +116,7 @@ DO_TRAPEZE_GENERAL(TRAPEZE_LOOP_UTD, TRAPEZE_LOOP_LTR,                        \
                    b, p0, p1, d,                                              \
                    t0, t1,                                                    \
                    x0, dx0, x1, dx1,                                          \
-                   y0, dy0, y1, dy1)                                          \
+                   y0, dy0, y1, dy1, ADDITIVE_LOOP)                                          \
 
 #define DO_TRAPEZE_RIGHT(b, p0, p1, d,                                        \
                            t0, t1,                                            \
@@ -108,7 +127,7 @@ DO_TRAPEZE_GENERAL(TRAPEZE_LOOP_UTD, TRAPEZE_LOOP_LTR,                        \
                    b, p0, p1, d,                                              \
                    t0, t1,                                                    \
                    x0, dx0, x1, dx1,                                          \
-                   y0, dy0, y1, dy1)                                          \
+                   y0, dy0, y1, dy1, ADDITIVE_LOOP)                                          \
 
 #define DO_TRAPEZE_BOTTOM_LEFT(b, p0, p1, d,                                  \
                            t0, t1,                                            \
@@ -119,7 +138,7 @@ DO_TRAPEZE_GENERAL(TRAPEZE_LOOP_UTD, TRAPEZE_LOOP_RTL,                        \
                    b, p0, p1, d,                                              \
                    t0, t1,                                                    \
                    x0, dx0, x1, dx1,                                          \
-                   y0, dy0, y1, dy1)                                          \
+                   y0, dy0, y1, dy1, SUBTRACTIVE_LOOP)                                          \
 
 
 #define DO_TRAPEZE_BOTTOM(b, p0, p1, d,                                       \
@@ -131,7 +150,7 @@ DO_TRAPEZE_GENERAL(TRAPEZE_LOOP_UTD, TRAPEZE_LOOP_LTR,                        \
                    b, p0, p1, d,                                              \
                    t0, t1,                                                    \
                    x0, dx0, x1, dx1,                                          \
-                   y0, dy0, y1, dy1)                                          \
+                   y0, dy0, y1, dy1, ADDITIVE_LOOP)                                          \
 
 #define DO_TRAPEZE_BOTTOM_RIGHT(b, p0, p1, d,                                 \
                            t0, t1,                                            \
@@ -142,5 +161,4 @@ DO_TRAPEZE_GENERAL(TRAPEZE_LOOP_UTD, TRAPEZE_LOOP_LTR,                        \
                    b, p0, p1, d,                                              \
                    t0, t1,                                                    \
                    x0, dx0, x1, dx1,                                          \
-                   y0, dy0, y1, dy1)                                          \
-
+                   y0, dy0, y1, dy1, ADDITIVE_LOOP)                                          \
